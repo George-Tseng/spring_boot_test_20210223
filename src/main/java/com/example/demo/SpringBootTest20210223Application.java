606d02d61be538385893abb362864fdcd130c7c2
GeorgeTseng017 <georgecycuphy@cycu.org.tw>
2021-02-25 15:41:31 +0800

    first upload by source tree

---
+++ src/main/java/com/example/demo/SpringBootTest20210223Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest20210223Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTest20210223Application.class, args);
+	}
+
+}
